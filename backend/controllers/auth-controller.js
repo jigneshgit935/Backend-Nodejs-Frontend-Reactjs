83095d93b4a1f8fcfbb470a4e8b45d991a0fc184
Jignesh <jigneshmal.com>
2025-02-02 22:07:08 +0530

    setup for backend

---
+++ backend/controllers/auth-controller.js
@@ -0,0 +1,11 @@
+export const signup = async (req, res) => {
+  res.send("sign up route is being called");
+};
+
+export const login = async (req, res) => {
+  res.send("login route is being called");
+};
+
+export const logout = async (req, res) => {
+  res.send("logout route is being called");
+};
